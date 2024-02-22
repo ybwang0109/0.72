5cb83a5df7dc6b587359a09f1d8c6df417e43393
lx <3212843782@qq.com>
2024-02-22 18:02:17 +0800

    entry外层文件及Reactopenharmony中部分文件迁徙

--- Reactopenharmony/src/main/cpp/react_arkui/TaskExecutor/AbstractTaskRunner.h
+++ Reactopenharmony/src/main/cpp/react_arkui/TaskExecutor/AbstractTaskRunner.h
@@ -1,1 +1,15 @@
-console.log('11111')
+#pragma once
+
+#include <functional>
+
+class AbstractTaskRunner {
+public:
+    using Task = std::function<void()>;
+
+    virtual void runAsyncTask(Task &&task) = 0;
+    virtual void runSyncTask(Task &&task) = 0;
+
+    virtual bool isOnCurrentThread() const = 0;
+
+    virtual ~AbstractTaskRunner() = default;
+};
--- Reactopenharmony/src/main/cpp/react_arkui/defaults/DefaultTurboModuleManagerDelegate.cpp
+++ Reactopenharmony/src/main/cpp/react_arkui/defaults/DefaultTurboModuleManagerDelegate.cpp
@@ -1,1 +1,26 @@
-console.log('11111')
+//
+// Created on 2024/1/30,
+//
+// Node APIs are not fully supported. To solve the compilation error of the interface cannot be found,
+// Please include "napi/native_api.h".
+
+#include "DefaultTurboModuleManagerDelegate.h"
+#include "react_core_package/turbo_module/rncore_ModuleProvider.h"
+
+namespace rnoh {
+    std::function<std::shared_ptr<TurboModule>(const std::string &, const ArkTSTurboModule::Context &)>
+        DefaultTurboModuleManagerDelegate::ArkTSModuleProvider{nullptr};
+    
+    DefaultTurboModuleManagerDelegate::DefaultTurboModuleManagerDelegate(){};
+
+    std::shared_ptr<facebook::react::TurboModule>
+    DefaultTurboModuleManagerDelegate::getTurboModule(ArkTSTurboModule::Context &ctx, const std::string &name) {
+        auto moduleProvider = DefaultTurboModuleManagerDelegate::ArkTSModuleProvider;
+        if (moduleProvider) {
+            if (auto resolveModule = moduleProvider(name, ctx)) {
+                return resolveModule;
+            }
+        }
+        return rncore_ModuleProvider(name, ctx);
+    }
+} // namespace rnoh
