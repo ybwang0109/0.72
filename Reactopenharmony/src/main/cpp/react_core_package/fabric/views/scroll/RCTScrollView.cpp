53ffca19c8bd1fda384f23d7730f1fd7d74f3ac2
wangyb <ybwangdp@isoftstone.com>
2024-02-27 15:55:36 +0800

    校对1

--- Reactopenharmony/src/main/cpp/react_core_package/fabric/views/scroll/RCTScrollView.cpp
+++ Reactopenharmony/src/main/cpp/react_core_package/fabric/views/scroll/RCTScrollView.cpp
@@ -2,6 +2,6 @@
 // Created on 2024/2/20.
 //
 // Node APIs are not fully supported. To solve the compilation error of the interface cannot be found,
-// please include "napi/native_api.h"
+// please include "napi/native_api.h".
 
-#include "RCTScrollView.h".
+#include "RCTScrollView.h"
